aff82b54b2331303807e4ed23dca02c50513057f
Rob Scott <rob@eventbooking.com>
2016-08-04 09:31:48 -0400

    Skip directories that end up too long

--- Scanner.cs
+++ Scanner.cs
@@ -3,47 +3,54 @@ using System.IO;
 
 namespace SourceCopyrightEnforcer
 {
-	public class Scanner
-	{
-		private readonly string _rootPath;
+    public class Scanner
+    {
+        private readonly string _rootPath;
 
-		public Scanner(string rootPath)
-		{
-			_rootPath = rootPath;
-		}
+        public Scanner( string rootPath )
+        {
+            _rootPath = rootPath;
+        }
 
-		public void Run()
-		{
-			var dirInfo = new DirectoryInfo(_rootPath);
-			if (dirInfo.Exists)
-			{
-				ScanDirectory(dirInfo);
-			}
-		}
+        public void Run()
+        {
+            var dirInfo = new DirectoryInfo( _rootPath );
+            if (dirInfo.Exists)
+            {
+                ScanDirectory( dirInfo );
+            }
+        }
 
-		private void ScanDirectory(DirectoryInfo dirInfo)
-		{
-			if (dirInfo.Name == "obj"
-			    || dirInfo.Name == "bin"
-			    || dirInfo.Name == "packages"
-			    || dirInfo.Name.StartsWith("."))
-				return;
+        private void ScanDirectory( DirectoryInfo dirInfo )
+        {
+            if (dirInfo.Name == "obj"
+                || dirInfo.Name == "bin"
+                || dirInfo.Name == "packages"
+                || dirInfo.Name.StartsWith( "." ))
+                return;
 
-			foreach (var file in dirInfo.EnumerateFiles())
-			{
-				if (file.Extension == ".cs")
-				{
-					Console.WriteLine(file.FullName);
-					var fu = new CSharpFileFixer(file);
-					if (fu.NeedsFix)
-						fu.Fix();
-				}
-			}
+            try
+            {
+                foreach (var file in dirInfo.EnumerateFiles())
+                {
+                    if (file.Extension == ".cs")
+                    {
+                        Console.WriteLine( file.FullName );
+                        var fu = new CSharpFileFixer( file );
+                        if (fu.NeedsFix)
+                            fu.Fix();
+                    }
+                }
 
-			foreach (var dir in dirInfo.EnumerateDirectories())
-			{
-				ScanDirectory(dir);
-			}
-		}
-	}
+                foreach (var dir in dirInfo.EnumerateDirectories())
+                {
+                    ScanDirectory( dir );
+                }
+            }
+            catch (PathTooLongException)
+            {
+                Console.WriteLine( "Path was too long. Continuing." );
+            }
+        }
+    }
 }
